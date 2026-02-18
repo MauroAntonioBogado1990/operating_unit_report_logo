1b4c7fa10feae9ddfcc45b26e01e2c155ef9b8cb
Mauro Bogado <54823116+MauroAntonioBogado1990@users.noreply.github.com>
2026-02-18 11:40:04 -0300

    Rename 'Unidad Operativa' to 'Unidad Operacional'

--- models/account_move.py
+++ models/account_move.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    operating_unit_id = fields.Many2one('operating.unit', string='Unidad Operativa')
+    operating_unit_id = fields.Many2one('operating.unit', string='Unidad Operacional')
