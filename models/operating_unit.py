7941c527b1fbb1c8dd1a4c92469852e157f15fb1
Mauro Bogado <54823116+MauroAntonioBogado1990@users.noreply.github.com>
2026-02-18 11:29:55 -0300

    Rename report_logo field to 'Logo de Reporte'

--- models/operating_unit.py
+++ models/operating_unit.py
@@ -4,5 +4,5 @@ from odoo import models, fields
 class OperatingUnit(models.Model):
     _inherit = "operating.unit"
 
-    report_logo = fields.Binary("Report Logo", attachment=True,
+    report_logo = fields.Binary("Logo de Reporte", attachment=True,
                                help="Logo used in reports for this operating unit. Falls back to company logo.")
