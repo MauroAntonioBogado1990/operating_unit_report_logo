c02ec0b2f0703b974d664bd700fcc8d787fe1d99
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2026-02-23 14:46:57 -0300

    add static js

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from . import operating_unit
 from . import pos
 from . import sale
 from . import account_move
+from . import account_journal
--- models/pos.py
+++ models/pos.py
@@ -1,11 +1,27 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
-    operating_unit_id = fields.Many2one('operating.unit', string='Unidad Operacional')
+    operating_unit_id = fields.Many2one('operating.unit', string='Unidad Operativa')
+    operating_unit_report_logo = fields.Binary(related='operating_unit_id.report_logo', readonly=True)
+    operating_unit_partner_image = fields.Binary(related='operating_unit_id.partner_id.image_1920', readonly=True)
+
+    @api.onchange('journal_id')
+    def _onchange_journal_operating_unit(self):
+        if self.journal_id and self.journal_id.operating_unit_id:
+            self.operating_unit_id = self.journal_id.operating_unit_id
 
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
-    operating_unit_id = fields.Many2one('operating.unit', string='Unidad Operacional', readonly=True)
+    operating_unit_id = fields.Many2one('operating.unit', string='Unidad Operativa', readonly=True)
+
+    @api.model
+    def create(self, vals):
+        # Propagate operating unit from pos config if not provided
+        if not vals.get('operating_unit_id') and vals.get('config_id'):
+            config = self.env['pos.config'].browse(vals.get('config_id'))
+            if config and config.operating_unit_id:
+                vals['operating_unit_id'] = config.operating_unit_id.id
+        return super(PosOrder, self).create(vals)
